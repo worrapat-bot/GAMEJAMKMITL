2a033a57d1ca4cfb826ada5a6719d6fb5f918c56
Mr.Kyu <qkabkab@gmail.com>
2025-11-08 21:35:52 +0700

    put

---
+++ Assets/Kyu/Code/CameraFollow.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; // เป้าหมายที่กล้องจะติดตาม
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
